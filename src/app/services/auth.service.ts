ad61027b1488e71b24c9319c740a96b4a2bf37c5
Diego Campos <camposbuenodiego1@gmail.com>
2024-04-25 23:38:48 -0500

    SCRUM-5 login-interfaces-fn

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthenticationLoginDTORequest } from '../interfaces/AuthenticationLoginDTORequest ';
 import { AuthenticationSignUpDTORequest } from '../interfaces/AuthenticationSignUpDTORequest';
 
 @Injectable({
@@ -15,6 +16,10 @@ export class AuthService {
 
   signUp(signUpDTORequest: AuthenticationSignUpDTORequest): Observable<any> {
     return this.http.post(`${this.baseUrl}/cuenta/signup`, signUpDTORequest);
+  }
+
+  login(loginDtoRequest: AuthenticationLoginDTORequest): Observable<any> {
+    return this.http.post(`${this.baseUrl}/cuenta/login`, loginDtoRequest);
   }
 
   confirmation(token: string): Observable<any> {
